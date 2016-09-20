0b7e3fcb09b3e7e1846ce981265333f7bcb84748
gerdFace <stevenagerdes@gmail.com>
2016-09-19 23:22:46 -0500

    Add files via upload

--- GuessNum.java
+++ GuessNum.java
@@ -13,39 +13,42 @@ public class GuessNum {
         System.out.println("I'm thinking of a number from 1 to 100.");
         System.out.println("Can you guess what it is in 5 tries?");
 
-        gameCounter(userin, theNum, guess, count);
-    }
-
-    private static void gameCounter(Scanner userin, int theNum, int guess, int count) {
-        while (guess != theNum) {
+        while (true) {
             guess = userin.nextInt();
             count++;
-            gameRules(theNum, guess, count);
-            if (count == 4) {
-                System.out.println("Last try!");
-            }
-            if (count >= 5) {
-                System.out.println("You are out of guesses.");
-                System.out.println("The number was " + theNum + ".");
-                break;
-            }
+            winGame(theNum, guess, count);
+            loseGame(theNum, guess, count);
+            helpUser(guess, theNum, count);
         }
     }
 
-    private static void gameRules(int theNum, int guess, int count) {
+    private static boolean loseGame(int theNum, int guess, int count) {
+        if ((count >= 5) && (guess != theNum)) {
+            System.out.println("You are out of guesses.");
+            System.out.println("The number was " + theNum + ".");
+        }
+        return false;
+    }
+
+    private static void winGame(int theNum, int guess, int count) {
+        if (guess == theNum) {
+            System.out.println("Congratulations. You guessed the number with "
+                    + count + " tries!");
+        }
+    }
+
+    private static void helpUser(int guess, int theNum, int count) {
         if (guess > theNum) {
             System.out.println("lower!");
         } else if (guess < theNum) {
             System.out.println("higher!");
-        } else if (guess == theNum) {
-            System.out.println("Congratulations. You guessed the number with "
-                    + count + " tries!");
         }
-        if (guess > 100) {
-            System.out.println("The number is between 1 and 100.");
+        if (count == 4) {
+            System.out.println("Last try!");
         }
-        if (guess < 0) {
+        if ((guess > 100) || (guess < 0)) {
             System.out.println("The number is between 1 and 100.");
         }
     }
 }
+
