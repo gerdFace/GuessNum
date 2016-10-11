70e1e7bb664ccd67301abdb0df35a2d41cf3da5c
gerdFace <stevenagerdes@gmail.com>
2016-10-11 13:39:37 -0500

    Add files via upload

--- GameLauncher.java
+++ GameLauncher.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class GameLauncher {
 
-    public static int count = 0;
-    public static int guess;
+    private static int count = 0;
+    private static int guess;
     private static int theNum;
     private static GameRules game;
     private static Random rand;
@@ -15,12 +15,11 @@ public class GameLauncher {
         rand = new Random();
         theNum = rand.nextInt(100) + 1;
         game = new GameRules();
-        guess = userIn.nextInt();
-
         System.out.println("I'm thinking of a number from 1 to 100.");
         System.out.println("Can you guess what it is in 5 tries?");
 
         while (count < 5) {
+            guess = userIn.nextInt();
             count++;
             game.helpUser(guess, theNum, count);
             game.loseGame(theNum, guess, count);
