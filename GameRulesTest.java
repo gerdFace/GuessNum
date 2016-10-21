8d481a623cb340203beee1080cff7629a6da68fa
gerdFace <stevenagerdes@gmail.com>
2016-10-21 17:23:29 -0500

    Add files via upload

--- GameRulesTest.java
+++ GameRulesTest.java
@@ -1,15 +1,14 @@
-//import main.GameRules;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class GameRulesTest {
-//
-//    @Test
-//    public void canLoseGame() {
-//        GameRules gameRules = new GameRules();
-//
-//        gameRules.loseGame(int theNum, int guess, int count);
-//
-//        Assert.assertEquals(50, 4, 4);
-//    }
-//}
+import main.GameRules;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameRulesTest {
+
+    @Test
+    public void canLoseGame() {
+        GameRules gameRules = new GameRules();
+        String result = "You Lose";
+
+        Assert.assertEquals(result, gameRules.loseGame(5, 5, 10));
+    }
+}
